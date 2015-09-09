5c9b609c6541a7a69428d1e59d7628bbf41c220b
Marc Jesse <marcjesse92@hotmail.com>
2015-09-09 09:51:35 +0200

    Updated messageProcessor
    You now cannot message yourself, but if you try to message yourself and
    another client, the server will send the message to the client, but not
    to you.

--- src/ca/server/MessageProcessor.java
+++ src/ca/server/MessageProcessor.java
@@ -22,13 +22,17 @@ public class MessageProcessor {
 
     public void process(String message) {
         //if message starts with MSG#, remove MSG#-tag. Find cleints and send message to them.
-        if (message.startsWith("MSG#")||message.startsWith("STOP#")) {
+        if (message.startsWith("MSG#") || message.startsWith("STOP#")) {
             message = message.replace("MSG#", "");
             findUsers(message);
-        }else origin.send("Wrong syntax!");
-        
+        } else {
+            origin.send("Wrong syntax!");
+        }
+
     }
+
     //TODO, MAKE SURE THAT PERSONS CANT SEND MESSAGES TO A RECIEPIENT MORE THAN ONCE! EX. MSG#USER1,USER1#MESSAGE
+
     private void findUsers(String message) {
         //Message should look like user,user#Message
         //first split at # to get user,user
@@ -36,7 +40,11 @@ public class MessageProcessor {
         String[] split = message.split("#");
         //split[0] = user,user & split[1]message
         String users = split[0];
-        message = split[1];
+        try {
+                message = split[1];
+        } catch (Exception e) {
+        }
+
         //if message contains more than one #, it must be part of the message.
         //The Split[] will therefore have index greater than 2.
         //And we will need to add the # back into the message.
@@ -53,8 +61,6 @@ public class MessageProcessor {
             usersSplit = users.split(",");
         }
 
-        
-        
         //Get users
         ArrayList<ClientHandler> clientsInList = Server.getClientList();
         ArrayList<ClientHandler> clientRecievers = new ArrayList(); //make empty array to add reciepient in.
@@ -66,10 +72,16 @@ public class MessageProcessor {
                 }
             }
         }
-        if(clientRecievers.contains(origin)){
+        if (clientRecievers.contains(origin)) {
             clientRecievers.remove(origin);
             origin.send("Info: You can't message yourself.");
+            if(clientRecievers.size() >= 1){
+                origin.send(origin.getClientName() + ": " + message);
+            }
+        } else {
+            origin.send(origin.getClientName() + ": " + message);
         }
-        Server.sendToUsers(clientRecievers, origin.getClientName()+": "+message); //send messages to users.
+        Server.sendToUsers(clientRecievers, origin.getClientName() + ": " + message); //send messages to users.
+
     }
 }
